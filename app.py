7d103fb4121d08e504552867fd7b60b93df91154
RobPruzan <97781863+RobPruzan@users.noreply.github.com>
2022-08-08 12:16:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -256,16 +256,14 @@ def gradio_fn(text, audio, target, actual_audio):
     if text is None and audio is None and target is None and actual_audio is None:
         return "No Inputs", "No Inputs", "No Inputs", "No Inputs"
     speech_score = 0
-    div = calculate_diversity(text)
 
     if actual_audio is not None:
         actual = p(actual_audio)["text"]
-        print('sdfgs')
         speech_score = compute_score(target, actual)
 
         return "Difficulty Score: " + str(reading_difficulty(actual)), "Transcript: " + str(
-            actual.lower()), "Diversity Score: " + str(div[1]), "Speech Score: " + str(speech_score)
-
+            actual.lower()), "Diversity Score: " + str(calculate_diversity(target)[1]), "Speech Score: " + str(speech_score)
+    div = calculate_diversity(text)
     transcription = []
     if audio is not None:
         text = p(audio)["text"]
@@ -320,8 +318,23 @@ interface = gr.Interface(
     outputs=["text", "text", "text", "text"],
     theme="huggingface",
     description="Enter text or speak into your microphone to have your text analyzed!",
-
     rounded=True,
-    container=True
-
+    container=True,
+    article="""
+    Fine-Tuned Distil Bert- Automatically determining how difficult something is to read is a difficult task as underlying semantics are relevant. 
+    To efficiently compute text difficulty, a Distil-Bert pre-trained model is fine-tuned for regression using The CommonLit Ease of Readability (CLEAR) 
+    Corpus. https://educationaldatamining.org/EDM2021/virtual/static/pdf/EDM21_paper_35.pdf This dataset contains over 110,000 pairwise comparisons of 
+    ~1100 teachers responded to the question, "Which text is easier for students to understand?". This model is trained end-end (regression layer down to 
+    the first attention layer to ensure the best performance- Merchant et al. 2020
+    
+    Speech Pronunciaion Scoring: The Wave2Vec 2.0 model is utilized to convert audio into text in real-time. The model predicts words or phonemes (smallest 
+    unit of speech distinguishing one word (or word element) from another) from the input audio from the user. Due to the nature of the model, users with poor 
+    pronunciation get inaccurate results. This project attempts to score pronunciation by asking a user to read a target excerpt into the microphone. We then
+    pass this audio through Wave2Vec to get the inferred intended words. We measure the loss as the Levenshtein distance between the target and actual transcripts- 
+    the Levenshtein distance between two words is the minimum number of single-character edits required to change one word into the other.
+    Lexical Diversity Score: The lexical diversity score is computed by taking the ratio of unique similar words to total similar words squared. The similarity is computed 
+    as if the cosine similarity of the word2vec embeddings is greater than .75. It is bad writing/speech practice to repeat the same words when it's possible not to. 
+    Vocabulary diversity is generally computed by taking the ratio of unique strings/ total strings. This does not give an indication if the person has a large vocabulary 
+    or if the topic does not require a diverse vocabulary to express it
+    """
 ).launch()
